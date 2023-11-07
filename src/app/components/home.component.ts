8fa151d6472647a270a617f11793c9d3d2708952
Alec Rawlins <alec12999@yahoo.com>
2023-11-06 19:19:27 -0500

    added hero and bottom images to page

--- src/app/components/home.component.ts
+++ src/app/components/home.component.ts
@@ -7,8 +7,10 @@ import { Component } from '@angular/core';
     <app-heading></app-heading>
   </header>
   <main>
+    <app-hero></app-hero>
     <app-main-slides></app-main-slides>
     <app-testimonials></app-testimonials>
+    <app-bottom-images></app-bottom-images>
   </main>
   <footer>
     <app-footer></app-footer>
