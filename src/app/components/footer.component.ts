808ddabdd01c6eafb205944d962e026ae7f10864
Alec Rawlins <alec12999@yahoo.com>
2023-11-06 19:18:54 -0500

    social icons and styles

--- src/app/components/footer.component.ts
+++ src/app/components/footer.component.ts
@@ -9,8 +9,34 @@ import { Component } from '@angular/core';
     <a href="">Services</a>
     <a href="">Projects</a>
   </div>
+  <div class="socials">
+    <a href="https://www.facebook.com/" target="_blank"><svg class="icon"><use href="#facebookIcon"></use></svg></a>
+    <a href="https://www.instagram.com/" target="_blank"><svg class="icon"><use href="#instagramIcon"></use></svg></a>
+    <a href="https://www.twitter.com/" target="_blank"><svg class="icon"><use href="#twitterIcon"></use></svg></a>
+    <a href="https://www.pinterest.com/" target="_blank"><svg class="icon"><use href="#pinterestIcon"></use></svg></a>
+  </div>
+
   `,
-  styles: [``]
+  styles: [`
+  h1 {
+    text-align: center;
+  }
+  .socials {
+    display: flex;
+    justify-content: center;
+    gap: 30px;
+  }
+  .icon {
+    width: 30px;
+    height: 30px;
+  }
+  #menu {
+    display: flex;
+    justify-content: center;
+    gap: 40px;
+    margin: 30px 0 60px 0;
+  }
+  `]
 })
 export class FooterComponent {
 
